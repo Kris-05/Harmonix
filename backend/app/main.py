ad91a3c4ed60d89f9dd050dfd07107e10fc85edb
SAKTHIPRIYAN1 <sakthipriyan417@gmail.com>
2025-03-29 01:05:52 +0530

    Bug hunt and back end

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,23 +1,42 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI,Request
-from app.routers import Accounts
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi_socketio import SocketManager
+import socketio
+from app.routers import Accounts, Gesture
 from app.db.database import init_db
+from app.services import Gesture as gs
+
+
+
+from app.sockets import sio_app
 app = FastAPI()
 
 
-# for starting uppp...
-# setting indec for Unique property......
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/socket.io", sio_app)
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Initializing database...")
-    await init_db()  # Create indexes, etc.
+    print(" Initializing database")
+    await init_db() 
     yield
-    print("Cleaning up resources")
+    print(" Cleaning up resources")
 
-# default root
+
 @app.get("/")
 def read_root():
-    return {"message": " FastAPI is working "}
+    return {"message": "FastAPI is working"}
 
-# adding the routers/....
+
 app.include_router(Accounts.router)
+app.include_router(Gesture.router)
--- backend/app/routers/Accounts.py
+++ backend/app/routers/Accounts.py
@@ -16,8 +16,7 @@ async def createAccount(user: UserModel):
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Account creation failed"
             )
-        
-        # Set the new user's ID and return the response
+
         user.id = user_id
         return user
 
@@ -32,7 +31,7 @@ async def createAccount(user: UserModel):
 
 
 # Login User Route
-@router.post("/login") 
+@router.post("/login")
 async def loginUser(login_data: LoginModel):
     email=login_data.email
     password=login_data.password
